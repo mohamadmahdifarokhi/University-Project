59c605d10657abadda69cdaf0d104f5ced5ad5f4
kooshachehreh <koosha.bagherzadeh@gmial.com>
2024-04-19 12:45:49 +0330

    device, pricinf and power records were added 4

--- university-project/back/src/device/services.py
+++ university-project/back/src/device/services.py
@@ -9,7 +9,6 @@ from fastapi.responses import JSONResponse
 
 def service_add_device(
     device: DeviceSchema,
-    db
 ):
     device = DeviceSchema(
         name=device.name,
@@ -23,7 +22,6 @@ def service_add_device(
 
 def service_delete_device(
     device_id: str,
-    db
 ):
     update_result = db["device"].delete_one(
         {"_id": ObjectId(device_id)},
@@ -34,19 +32,16 @@ def service_delete_device(
 def service_update_device(
     device_id: str,
     device_data: DeviceUpdateSchema,
-    db
 ):
     pass
 
 def service_device_get(
     device_id:str,    
-    db
 ):
     device = db["device"].find_one({"_id": ObjectId(device_id)})
     return device
 
 def service_list_device_all(
-    db
 ):
     devices = db["device"].find()
     results = []
@@ -59,7 +54,6 @@ def service_list_device_all(
 
 def service_list_device_user(
     user_id: str,
-    db
 ):
     devices = db["users"].find_one({"_id": ObjectId(user_id)})["devices"]
     results = []
--- university-project/back/src/pricing/api.py
+++ university-project/back/src/pricing/api.py
@@ -11,28 +11,22 @@ router = APIRouter(
 
 @router.get("/", summary="Get all pricings")
 def pricing_list_all(
-    db=db
 ):
     return service_get_all_pricing(
-        db=db
     )
 
 @router.post("/add", summary="adds pricing")
 def pricing_add(
     pricing: PricingSchema,
-    db=db
 ):
     return service_add_pricing(
         pricing,
-        db=db,
     )
 
 @router.delete("/delete", summary="deletes pricing")
 def pricing_delete(
-    pricing_id: str,
-    db=db
+    pricing_id: str
 ):
     return service_delete_pricing(
-        pricing_id,
-        db=db,
+        pricing_id
     )
