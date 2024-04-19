e5bff0a23fc6e6b4154b533ad18a4578fdd5c423
kooshachehreh <koosha.bagherzadeh@gmial.com>
2024-04-19 15:05:01 +0330

    device, pricinf and power records were added 9

--- university-project/back/src/power_record/api.py
+++ university-project/back/src/power_record/api.py
@@ -11,18 +11,18 @@ router = APIRouter(
 
 @router.get("/{user_id}/records", summary="Get all records of a user")
 def power_records_list_all(
-    user_id: str
+    user: User = Depends(get_current_user)
 ):
     return service_list_power_records(
-        user_id=user_id
+        user_id=user["_id"]
     )
 
 @router.post("/add-record", summary="adds a record to power records with user id")
 def power_record_add(
     power_record: PowerRecordAddSchema,
-    user_id: str
+    user: User = Depends(get_current_user)
 ):
     return service_add_power_records(
         power_record,
-        user_id=user_id
+        user_id=user["_id"]
     )
